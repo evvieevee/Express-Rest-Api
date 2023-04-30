3dd17930923676a8516e7fe0200e127e648a2b71
evvieevee <eve.puurtinen00@gmail.com>
2023-04-30 14:19:31 +0300

    adds comments

--- artist.js
+++ artist.js
@@ -1,35 +1,38 @@
+// Alustusta mongoose ja express osalta. 
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose');
 
+//määritellään  uusi schema joka tuo kappaleen.
 const songSchema = new mongoose.Schema({
   name: String
 });
-
+//Määritellään uusi schema, joka tuo artistin.
 const artistSchema = new mongoose.Schema({
   name: String,
   songs: [songSchema]
 });
 
-
+//määritellään model artistille
 const Artist = mongoose.model('Artist', artistSchema);
 
-
+// Määritellään reitti. Kun polku ladataan sovellus vastaa lähettämällä takaisin artist home tekstin. 
 router.get('/', (req, res) => {
   res.send('artist home')
 })
-
+// Hakee kaikki artistit ja lähettää niiden listan
 router.get('/getall', async (req, res) => {
   const list = await Artist.find();
   res.send(list)
 })
-
+// Hakee artistin id:n avulla.
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
   const list = await Artist.findById(id);
   res.send(list)
 })
-
+// post- funktio, jossa pyritään lisäämään uusi artisti ja tarkitsetaan virheiden varalta ettei nimi ole tyhjä.
+//jos miltään puuttuu nimi server lähettää koodin 500 ja kertoo mitkä puuttuu. 
 router.post('/add', async (req, res) => {
   const artistName = req.body.artistName;
   const songs = req.body.songs;
@@ -47,14 +50,14 @@ router.post('/add', async (req, res) => {
     res.status(500).send("missing name")
   }
 })
-
+// Poistaa artistin id avulla
 router.delete('/delete/:id', async (req, res) => {
   const id = req.params.id;
   await Artist.deleteOne({ _id: id }).then((x => {
     res.send(x)
   })).catch(err => res.send(err))
 })
-
+// Päivittää yhden artistin id avulla.
 router.patch('/update/:id', async (req, res) => {
   const id = req.params.id;
   const update = req.body;
@@ -62,4 +65,5 @@ router.patch('/update/:id', async (req, res) => {
   res.send(updated);
 })
 
+//exportataan tietoja toiseen tiedostoon.
 module.exports = router
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+//Portti 3000 avattu
 const express = require('express')
 const app = express()
 const port = 3000
-
+// ladataan mongoose paketti, ladataan artist.js tiedosto, 
+// ladataan bodyparser paketti ja dotenv paketti. 
 const mongoose = require('mongoose');
 const artistRoutes = require('./artist');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
 main().catch(err => console.log(err));
-
+// yhdistetään mongodb tietokantaan. Odotetaan await funktion avulla, että yhdistytään, ennenkuin jatketaan koodissa eteenpäin.
+// Yhdistetään .env tiedostossa olevilla tunnuksilla tietokantaan. 
+// ilmoitetaan "atlas ready", kun on yhdistetty tietokantaan yhteys. ja tarkistetaan, että jos ei yhdisty, niin ilmoitetaan siitä. 
 async function main() {
   await mongoose.connect('mongodb+srv://'+ process.env.MONGO_USERNAME + ':'+ process.env.MONGO_PASSWORD +'@cluster0.fddad9u.mongodb.net/?retryWrites=true&w=majority')
   .then(async () => {
@@ -17,16 +21,17 @@ async function main() {
 
  
 }
-
+// Muutetaan tiedot json- muotoon ja ohjataan /api polut.
 app.use(bodyParser.json());
 app.use('/api', artistRoutes)
-
+// Lähettää vastauksen "hello world", kun polulle saavutaan.
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+ // Kuuntelee porttia ja console log, joka ilmoittaa portin jota aplikaatio kuuntelee. 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 
+ 
